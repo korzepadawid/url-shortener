d5c2419c80486f1c84800e303123eb0220078693
korzepadawid <korzepadawid@yahoo.com>
2021-08-14 17:38:09 +0200

    feat: added Exception handling for JSON parse error

--- src/main/java/io/github/korzepadawid/urlshortener/api/v1/controllers/RestControllerExceptionHandler.java
+++ src/main/java/io/github/korzepadawid/urlshortener/api/v1/controllers/RestControllerExceptionHandler.java
@@ -3,11 +3,11 @@ package io.github.korzepadawid.urlshortener.api.v1.controllers;
 import io.github.korzepadawid.urlshortener.api.v1.models.RestException;
 import io.github.korzepadawid.urlshortener.exceptions.ResourceNotFoundException;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RestControllerExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<Object> handleResourceNotFound(Exception exception) {
+  public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException exception) {
     log.error(exception.getLocalizedMessage());
 
     HttpStatus httpStatus = HttpStatus.NOT_FOUND;
@@ -42,6 +42,18 @@ public class RestControllerExceptionHandler {
     RestException restException = new RestException("Validation error.",
         httpStatus.value(), errors);
 
-    return new ResponseEntity<>(restException, HttpStatus.BAD_REQUEST);
+    return new ResponseEntity<>(restException, httpStatus);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<Object> handleHttpMessageNotReadable(
+      HttpMessageNotReadableException exception) {
+    log.error("JSON Parse error.");
+
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    RestException restException = new RestException("JSON Parse error.",
+        httpStatus.value(), null);
+
+    return new ResponseEntity<>(restException, httpStatus);
   }
 }
