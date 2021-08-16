c5a69fb624ab62c18c84f3c7ffe080baf1d840ac
korzepadawid <korzepadawid@yahoo.com>
2021-08-16 16:48:59 +0200

    feat: integration tests for UrlController HTTP POST

--- src/test/java/io/github/korzepadawid/urlshortener/api/v1/controllers/UrlControllerIntegrationTest.java
+++ src/test/java/io/github/korzepadawid/urlshortener/api/v1/controllers/UrlControllerIntegrationTest.java
@@ -4,10 +4,13 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import io.github.korzepadawid.urlshortener.JsonMapper;
+import io.github.korzepadawid.urlshortener.api.v1.models.UrlWriteDto;
 import io.github.korzepadawid.urlshortener.config.JacksonDateTimeConfig;
 import io.github.korzepadawid.urlshortener.models.Url;
 import io.github.korzepadawid.urlshortener.repositories.UrlRepository;
@@ -18,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -82,6 +86,60 @@ class UrlControllerIntegrationTest {
         .andExpect(jsonPath("$.shortUrl", is(shortUrl)))
         .andExpect(jsonPath("$.expiringAt", is(url.getExpiringAt()
             .format(DateTimeFormatter.ofPattern(JacksonDateTimeConfig.DATE_PATTERN)))));
+  }
+
+  @Test
+  void httpPost_WhenDateInvalidPattern_ThenReturnsJsonParseErrorAndStatus400() throws Exception {
+    mockMvc.perform(post(UrlController.BASE_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(
+            "{ \"url\": \"https://start.spring.io/\", \"expiringAt\": \"20-JUN-2025 08:03\" }"))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message", is("JSON Parse error.")));
+  }
+
+  @Test
+  void httpPost_WhenExpiringATFromThePast_ThenReturnsValidationErrorAndStatus400()
+      throws Exception {
+    UrlWriteDto urlWriteDto = UrlWriteDto.builder()
+        .url(HTTPS_STACKOVERFLOW_COM)
+        .expiringAt(LocalDateTime.now().minusSeconds(1))
+        .build();
+
+    mockMvc.perform(post(UrlController.BASE_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(JsonMapper.toJson(urlWriteDto)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.details.expiringAt", containsString("must be from the future")));
+  }
+
+  @Test
+  void httpPost_WhenInvalidUrl_ThenReturnsValidationErrorAndStatus400() throws Exception {
+    UrlWriteDto urlWriteDto = UrlWriteDto.builder()
+        .url("httXps://github.com/korzepadawid/url-shortener")
+        .build();
+
+    mockMvc.perform(post(UrlController.BASE_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(JsonMapper.toJson(urlWriteDto)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.details.url", containsString("Invalid url.")));
+  }
+
+  @Test
+  void httpPost_WhenExistingUrlWithTheSameExpiringAt_ThenReturnsOldInsteadOfNew() throws Exception {
+    Url url = urlEntityWithExpiringAt(null);
+    UrlWriteDto urlWriteDto = UrlWriteDto.builder().url(url.getUrl()).expiringAt(null).build();
+    Url savedUrl = urlRepository.save(url);
+    final String shortUrl = "/" + base62Service.encode(savedUrl.getId());
+
+    mockMvc.perform(post(UrlController.BASE_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(JsonMapper.toJson(urlWriteDto)))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.shortUrl", is(shortUrl)))
+        .andExpect(jsonPath("$.longUrl", is(urlWriteDto.getUrl())))
+        .andExpect(jsonPath("$.expiringAt", nullValue()));
   }
 
   Url urlEntityWithExpiringAt(LocalDateTime expiringAt) {
